c48952571f7e1335f756fd1b8ca4b5f15cd16e0c
georgewalker123 <georgewalker123@users.noreply.github.com>
2022-02-13 13:13:36 +0000

    UPDATE TO LATST SPOTIPI

--- python/displayCoverArt.py
+++ python/displayCoverArt.py
@@ -14,11 +14,11 @@ if len(sys.argv) > 2:
     username = sys.argv[1]
     token_path = sys.argv[2]
 
-    # Configuration file    
+    # Configuration file
     dir = os.path.dirname(__file__)
     filename = os.path.join(dir, '../config/rgb_options.ini')
 
-    # Configures logger for storing song data    
+    # Configures logger for storing song data
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='spotipy.log',level=logging.INFO)
     logger = logging.getLogger('spotipy_logger')
 
@@ -38,25 +38,34 @@ if len(sys.argv) > 2:
     options.hardware_mapping = config['DEFAULT']['hardware_mapping']
     options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
     options.brightness = int(config['DEFAULT']['brightness'])
-    options.led_rgb_sequence = config['DEFAULT']['led_rgb_sequence']
-    
+    #options.limit_refresh_rate_hz = int(config['DEFAULT']['refresh_rate'])
+
     default_image = os.path.join(dir, config['DEFAULT']['default_image'])
     print(default_image)
     matrix = RGBMatrix(options = options)
-    
+
+    prevSong    = ""
+    currentSong = ""
+
     try:
       while True:
         try:
           imageURL = getSongInfo(username, token_path)[1]
-          response = requests.get(imageURL)
-          image = Image.open(BytesIO(response.content))
-          image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-          matrix.SetImage(image.convert('RGB'))
+          currentSong = imageURL
+
+          if ( prevSong != currentSong ):
+            response = requests.get(imageURL)
+            image = Image.open(BytesIO(response.content))
+            image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+            matrix.SetImage(image.convert('RGB'))
+            prevSong = currentSong
+
           time.sleep(1)
-        except:
+        except Exception as e:
           image = Image.open(default_image)
           image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
           matrix.SetImage(image.convert('RGB'))
+          print(e)
           time.sleep(1)
     except KeyboardInterrupt:
       sys.exit(0)
